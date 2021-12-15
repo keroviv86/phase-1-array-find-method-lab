6666bbd556d3fed7a97f3a0deb35fc097956ca31
Vivian Yim <keroviv86@gmail.com>
2021-12-15 11:32:48 -0600

    commit tree fcb8c6a4c454d5ea20778ee1e7f6a99e607a00d4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 
+// function superbowlWin(record){
+//     let winningYear = record.find((item) => item.result ==='W')
+//     if (winningYear){
+//         return winningYear.year
+//     }    
+// }
+
+// superbowlWin(record)
+
+function superbowlWin(record){
+    let yearWon= record.find(function(element){
+        return element.result === "W"
+    })
+    if(yearWon){
+        return yearWon.year
+    }
+}
+
+superbowlWin(record)
+// function superbowlWin(record){
+//     return record.find(item => item.result === "W")
+// function superbowlWin(array, finder){
+//     let result = null
+//     for(let item of array){
+//         if(finder(item) ===true){
+//             result = item
+//             break
+//         }
+//     }
+//     return result
+// }
+
+
+// function isW(w){
+//     return w.result === 'W'
+// }
+
+
+//will loop through array
+//find first object whose condition returns true based on a callback
